487d56c84fd29d5b1c52dfbd78ae3f0ad7fc7474
Kaeland Chatman <kaeland1@gmail.com>
2018-12-12 15:46:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const getFirstSelector = function(selector) {
+  return document.querySelector(selector);
+};
 
+const nestedTarget = function() {
+  return document.querySelector('#nested .target');
+};
+
+const increaseRankBy = function(n) {
+  let rankedListNodes = document.querySelectorAll('ul.ranked-list li');
+
+  for (let node of rankedListNodes) {
+    node.innerHTML = (parseInt(node.innerHTML) + n).toString();
+  };
+};
+
+const deepestChild = function() {
+  let grandNode = document.querySelector('#grand-node');
+  let grandDivs = grandNode.getElementsByTagName('div');
+  let divString = '';
+  for (let i = 0; i <= grandDivs.length; i++) {
+    divString = divString + 'div ';
+  };
+  return grandNode.querySelector(divString.trim());
+};
